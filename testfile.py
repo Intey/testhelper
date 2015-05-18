25b916e509934cc98986456ed704c45f4a8f12ff
Intey <ziexe0@gmail.com>
2015-05-19 02:41:18 +0300

    cleaning.

--- testfile.py
+++ testfile.py
@@ -10,10 +10,6 @@ class TestFile:
         if case_name != "":
             for i in range(0, file_lines.__len__()):
                 if file_lines[i].__contains__(TestFile.caseName):
-                    file_lines[i] = TestFile.change_case_name(file_lines[i], case_name)
+                    file_lines[i] = file_lines[i].replace(TestFile.caseName, case_name)
 
         return file_lines
-
-    @staticmethod
-    def change_case_name(line, case_name):
-        return line.replace(TestFile.caseName, case_name)
