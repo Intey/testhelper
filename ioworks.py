6ddb5955811c0961c61717d8e7218459df324205
Churin Dmitry <churin@10205.ru>
2015-12-29 20:42:01 +0300

    [X|up] ~~barspin-flip~~ Add find_regex_in_file, and python2-python3 Xplatform

--- ioworks.py
+++ ioworks.py
@@ -3,13 +3,27 @@ def get_lines(path):
     :param path: filepath to read.
     :return: readed lines
     """
-    f = open(path, 'r', encoding="UTF8")
+    f = open(path, 'r')
     res = f.readlines()
     f.close()
     return res
 
 
 def write_to_file(path, lines):
-    file = open(path, 'w', encoding="UTF8")
+    file = open(path, 'w')
     file.writelines(lines)
     file.close()
+
+def find_pos_in_file(path, regex):
+    """Find line, where regex first time accure.
+    :return: line number, where regex found in first time"""
+    if regex == "":
+        return 0
+    import re
+    f = open(path, 'r')
+    for n,l in f:
+        if re.match(regex, line):
+            return n + 1 # inc, 'couze user count from 1, not 0
+            break
+    return 0
+    f.close()
